aabb2a0ca762743c54ee651b283e22b9893fa1ee
Abdrasul <abdrasulmihalko@gmail.com>
2022-04-15 16:22:13 +0600

    feat: added login and registration pages

--- render-api/main.go
+++ render-api/main.go
@@ -11,12 +11,21 @@ func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("templates/*")
 	router.Static("/assets", "./assets")
+	router.Static("/assets2", "./assets2")
 
 	httpClt := http.DefaultClient
 
 	//router.Handle("GET", "/index", func(context *gin.Context) {
 	//	context.HTML(http.StatusOK, "index.html", "")
 	//})
+
+	router.GET("/login", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "login.html", gin.H{})
+	})
+
+	router.GET("/registration", func(context *gin.Context) {
+		context.HTML(http.StatusOK, "registration.html", gin.H{})
+	})
 
 	router.GET("/index", func(c *gin.Context) {
 		resp, err := httpClt.Get("http://localhost:8080/challenge")
